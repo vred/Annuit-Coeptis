60e24fe704200f750e1939b501e203d3efcf7b89
Jeff Rabinowitz <rabinowitzjeffc@gmail.com>
2013-02-10 22:38:03 -0500

    Initialized empty Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnnuitCoeptis::Application.config.secret_token = '37605a416da6faf45db0b2372adefe9bfd84efec5a7114058b7caf2ca2831185465baca683934ff5326ff1ede60925faeb6312c7921a6fd1250f0ebbd4f9154f'
