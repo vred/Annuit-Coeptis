f1cbae22189a17f011d53267b9f3dffb67f85485
Jeff Rabinowitz <rabinowitzjeffc@gmail.com>
2013-02-15 17:16:52 -0500

    Used Devise to gen automagical signin/up/out for Users

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module AnnuitCoeptis
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # For Heroku
+    config.assets.initialize_on_precompile = false
+
   end
 end
