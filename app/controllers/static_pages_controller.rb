3a4d034154e3a0fb1a6e5c74b6227f0270f8e7b7
eric <ecuiffo@gmail.com>
2013-04-02 11:59:51 -0400

    Added dashboard and learn pages along with new routing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,28 @@
 class StaticPagesController < ApplicationController
-
   def home
-  	  @title = "home"
+    if current_user
+      redirect_to dashboard_path
+    end
+  	@title = "Home"
+  end
+
+  def dashboard
+    @title = "Dashboard"
+  end
+  
+  def learn
+    @title = "Learn"
   end
 
   def about
+    @title = "About"
   end
 
   def help
+    @title = "Help and FAQ"
   end
 
   def contact
+    @title = "Contact Us"
   end
 end
