34cfd4aad5adcc1e16ace3baae54d719fdf70dc7
Dario Rethage <dario@rethage.net>
2013-05-07 22:55:19 -0400

    Orders updated to create performance objects, start time bug fix

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -3,9 +3,9 @@ class Performance < ActiveRecord::Base
   require 'money-rails'
   attr_accessible :date, :closing_value, :closing_capital, :closing_margin, :league_id, :portfolio_id
   belongs_to :league
-  belongs_to portfolio
+  belongs_to :portfolio
   
-  monetize :capital_cents, :numericality => { :greater_than_or_equal_to => 0 }
-  monetize :margin_cents, :numericality => { :greater_than_or_equal_to => 0 }
+  monetize :closing_capital_cents, :numericality => { :greater_than_or_equal_to => 0 }
+  monetize :closing_margin_cents, :numericality => { :greater_than_or_equal_to => 0 }
   
 end
