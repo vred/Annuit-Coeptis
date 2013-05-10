f7ec52419747a9f808434bf5adf82e1e0f659153
NickPal <shmozer@gmail.com>
2013-05-10 13:08:30 -0400

    added ranks

--- app/models/league.rb
+++ app/models/league.rb
@@ -39,4 +39,66 @@ class League < ActiveRecord::Base
       self.member_number >= self.member_limit unless self.member_limit == nil
   end
 
+  def self.find_rank(portfolio_id, league_id)
+
+    members = Portfolio.where("league_id = ?",league_id)
+
+    leaders = []
+
+    members.each do |m|
+
+      d_positions = Order.find_positions(m.id)
+
+      orders = Order.where("portfolio_id = ?",portfolio_id)
+      orders = orders.group_by{|x| x.ticker}
+      keys = orders.keys
+      data_by_key = {}
+      keys.each do |key|
+
+        finance_data = YahooFinance::get_historical_quotes_days(key, 30)
+        finance_data = finance_data.map{|new_data| [Time.parse(new_data[0]).to_i*1000, new_data[4].to_f]}.sort_by{|d| d[0]}
+        data_by_key[key] = finance_data
+
+      end
+
+      d_total = Performance.today(m.id)
+
+      prev = 0
+
+      d_positions.each do |d_position|
+
+        unless d_position.nil?
+          unless data_by_key[d_position[0]][0].nil?
+              d_total += d_position[1] * data_by_key[d_position[0]][0][1]
+              prev = d_total
+            else
+              d_total = prev
+            end
+
+        end
+
+      end
+
+      leaders.push([m,d_total])
+
+    end
+
+    leaders.sort_by{|x| x[1]}.reverse
+
+    x=1
+
+    leaders.each do |me|
+
+      if me[0].id == portfolio_id
+
+        return x
+
+      end
+
+      x += 1
+
+    end
+
+  end
+
 end
